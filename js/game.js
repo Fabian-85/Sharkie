cc3b7dd9225474be2dcf8d8946fa32761f1d251a
Fabian-85 <fabian.maier0805@hotmail.com>
2024-08-22 13:11:39 +0200

    add startscreen

--- js/game.js
+++ js/game.js
@@ -1,63 +1,74 @@
- let canvas; 
+let canvas;
 
-let world; 
+let world;
 let keyboard = new Keyboard();
 
-function init(){ 
-canvas = document.getElementById("canvas");
-world = new World(canvas,keyboard);
-setLevel();
-world.level = level1;
-world.draw();
-world.run();
-world.addBubble();
+function init() {
+  canvas = document.getElementById("canvas");
+  world = new World(canvas, keyboard);
+  setLevel();
+  world.level = level1;
+  world.draw();
+  world.run();
+  world.addBubble();
 }
 
-function playGame(){
-document.getElementById('canvas').classList.remove('d-none');
-document.getElementById('startscreen').classList.add('d-none');
-init();
+function playGame() {
+  init();
+  document.getElementById('canvas').classList.remove('d-none');
+  document.getElementById('startscreen').classList.add('d-none');
+}
+
+function showControlling(){
+  document.getElementById('controlling').classList.remove('d-none');
+  document.getElementById('startscreen').classList.add('d-none');
+}
+
+function closeControllingCenter(){
+  document.getElementById('startscreen').classList.remove('d-none');
+  document.getElementById('controlling').classList.add('d-none');
+  
 }
 
 
-document.addEventListener('keydown', (event)=> {    
-      if(event.keyCode == 65 || event.keyCode == 37){
-        keyboard.LEFT = true; 
-      }
-      if(event.keyCode == 68 || event.keyCode == 39){
-        keyboard.RIGHT = true; 
-      }
-      if(event.keyCode == 83 || event.keyCode == 40){
-        keyboard.DOWN = true; 
-      }
-      if(event.keyCode == 87 || event.keyCode == 38){
-        keyboard.UP = true; 
-      }
-      if(event.keyCode == 32){
-        keyboard.SPACE = true; 
-      }
-      if(event.keyCode == 77){
-        keyboard.M = true; 
-      }
- });
+document.addEventListener('keydown', (event) => {
+  if (event.keyCode == 65 || event.keyCode == 37) {
+    keyboard.LEFT = true;
+  }
+  if (event.keyCode == 68 || event.keyCode == 39) {
+    keyboard.RIGHT = true;
+  }
+  if (event.keyCode == 83 || event.keyCode == 40) {
+    keyboard.DOWN = true;
+  }
+  if (event.keyCode == 87 || event.keyCode == 38) {
+    keyboard.UP = true;
+  }
+  if (event.keyCode == 32) {
+    keyboard.SPACE = true;
+  }
+  if (event.keyCode == 77) {
+    keyboard.M = true;
+  }
+});
 
- document.addEventListener('keyup', (event)=> {    
-      if(event.keyCode == 65 || event.keyCode == 37){
-        keyboard.LEFT = false; 
-      }
-      if(event.keyCode == 68 || event.keyCode == 39){
-        keyboard.RIGHT = false; 
-      }
-      if(event.keyCode == 83 || event.keyCode == 40){
-        keyboard.DOWN = false; 
-      }
-      if(event.keyCode == 87 || event.keyCode == 38){
-        keyboard.UP = false; 
-      }
-      if(event.keyCode == 32){
-        keyboard.SPACE = false; 
-      }
-      if(event.keyCode == 77){
-        keyboard.M = false; 
-      }
- });
+document.addEventListener('keyup', (event) => {
+  if (event.keyCode == 65 || event.keyCode == 37) {
+    keyboard.LEFT = false;
+  }
+  if (event.keyCode == 68 || event.keyCode == 39) {
+    keyboard.RIGHT = false;
+  }
+  if (event.keyCode == 83 || event.keyCode == 40) {
+    keyboard.DOWN = false;
+  }
+  if (event.keyCode == 87 || event.keyCode == 38) {
+    keyboard.UP = false;
+  }
+  if (event.keyCode == 32) {
+    keyboard.SPACE = false;
+  }
+  if (event.keyCode == 77) {
+    keyboard.M = false;
+  }
+});
